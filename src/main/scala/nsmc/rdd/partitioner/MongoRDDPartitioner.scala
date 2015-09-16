8529b05af9e7c6a685da6cce1ec02c33eef1fe32
Naden <nadenf@colesfinancialservices.com.au>
2015-09-17 09:32:39 +1000

    Changed to use _id as partition key in all cases

--- src/main/scala/nsmc/rdd/partitioner/MongoRDDPartitioner.scala
+++ src/main/scala/nsmc/rdd/partitioner/MongoRDDPartitioner.scala
@@ -9,17 +9,7 @@ class MongoRDDPartitioner(val collectionConfig: CollectionConfig) extends nsmc.L
     collectionConfig.databaseName, collectionConfig.collectionName)
 
   def makePartitions(): Array[Partition] = {
-    val intervals = if (collectionConfig.connectorConf.splitIndexed && collectionConfig.indexedKeys.length > 0) {
-      logInfo(s"Partitioning collection '${collectionConfig.collectionName}' in database '${collectionConfig.databaseName}' with synthetic partitions")
-      ig.generateSyntheticIntervals(collectionConfig.connectorConf.splitSize, collectionConfig.indexedKeys)
-    } else if (collectionConfig.connectorConf.useShardChunks) {
-      logInfo(s"Partitioning collection '${collectionConfig.collectionName}' in database '${collectionConfig.databaseName}' with shard chunks")
-      ig.generate(collectionConfig.connectorConf.directToShards)
-    } else {
-      logInfo(s"NOT Partitioning collection '${collectionConfig.collectionName}' in database '${collectionConfig.databaseName}' -- producing single partition")
-      val interval = new MongoInterval(null, null, collectionConfig.connectorConf.getDestination())
-      Seq(interval)
-    }
+    val intervals = ig.generateSyntheticIntervals(collectionConfig.connectorConf.splitSize, Seq(("_id", 1)))
     val partitions = intervals.zipWithIndex map {
       case (interval, index) => {
         val p: Partition = new MongoRDDPartition(index, 0, interval)
